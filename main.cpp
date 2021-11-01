ff90ab9c511debc19e9626e7695aa7eca9fa56e8
Alif-krniawn <82496663+Alif-krniawn@users.noreply.github.com>
2021-11-01 18:47:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include <iostream>
+#include <GL/gl.h>
+#include <GL/glut.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void display_callback();
+void reshape_callback(int,int);
 
-int main(int argc, char** argv) {
+void init()
+{
+	glClearColor(1.0,1.0,0.5,1.0);
+}
+
+int main(int argc,char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitWindowSize(800,750);
+	glutCreateWindow("SNAKE");
+	glutDisplayFunc(display_callback);
+	glutReshapeFunc(reshape_callback);
+	init();
+	glutMainLoop();
 	return 0;
+}
+
+void display_callback()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+} 
+
+voidreshape_callback(int w, int h)
+{
+	glViewport(0,0,(GLsizei)w,(GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 }
