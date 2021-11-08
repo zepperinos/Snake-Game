1feae73a8d6c54af916d1fa96c7d9b077060078d
Galih Carlos Putra Siregar <83207216+zepperinos@users.noreply.github.com>
2021-11-08 16:22:28 +0700

    warna + grid
    progress 3

--- main.cpp
+++ main.cpp
@@ -1,36 +1,44 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include "game.h"
+
+#define COLUMNS 50
+#define ROWS 50
 
 void display_callback();
 void reshape_callback(int,int);
 
 void init()
 {
-	glClearColor(1.0,1.0,0.5,1.0);
+    glClearColor(1.0,1.0,0.5,1.0);
+    initGrid(COLUMNS,ROWS);
 }
 
 int main(int argc,char **argv)
 {
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutInitWindowSize(800,750);
-	glutCreateWindow("SNAKE");
-	glutDisplayFunc(display_callback);
-	glutReshapeFunc(reshape_callback);
-	init();
-	glutMainLoop();
-	return 0;
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(1000,1000);
+    glutCreateWindow("SNAKE");
+    glutDisplayFunc(display_callback);
+    glutReshapeFunc(reshape_callback);
+    init();
+    glutMainLoop();
+    return 0;
 }
 
 void display_callback()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
-} 
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawGrid();
+    glutSwapBuffers();
+}
 
-voidreshape_callback(int w, int h)
+void reshape_callback(int w, int h)
 {
-	glViewport(0,0,(GLsizei)w,(GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+    glViewport(0,0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0,COLUMNS,0.0,ROWS,-1.0,1.0);
+    glMatrixMode(GL_MODELVIEW);
 }
